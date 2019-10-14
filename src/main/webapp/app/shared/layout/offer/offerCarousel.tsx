b8d56a4f8c8cc8e94c17bad16eeca946f3820861
Vtincent Talloir <vincent.talloir@soat.fr>
2019-10-14 16:05:17 +0200

    add offer card

---
+++ src/main/webapp/app/shared/layout/offer/offerCarousel.tsx
@@ -0,0 +1,33 @@
+
+import React, { Component } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+
+export interface OfferCarouselProps {
+    pictures: Blob[]
+    autoPlay?: boolean
+}
+
+export const OfferCarousel = ({ pictures, autoPlay = false }: OfferCarouselProps) => {
+    return (
+        <Carousel autoPlay={autoPlay} infiniteLoop={true} stopOnHover={false} showArrows={false} showThumbs={false} showStatus={false}>
+            {
+                pictures.length === 0 && (
+                    <div>
+                        <img src="../../../../content/images/default.png" />
+                    </div>
+                )
+            }
+            {
+                pictures.map( pictureBlob => {
+                    const imgSrc= URL.createObjectURL( pictureBlob );
+                    (
+                        <div>
+                            <img src={imgSrc} />
+                        </div>
+                    )
+                })
+            }
+        </Carousel>
+    )
+};
