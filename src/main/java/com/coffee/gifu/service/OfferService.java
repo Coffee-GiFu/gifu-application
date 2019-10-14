56724925c266ce566d54ea3eb6e8a00ea2367cb3
Vtincent Talloir <vincent.talloir@soat.fr>
2019-10-14 10:17:50 +0200

    create route

--- src/main/java/com/coffee/gifu/service/OfferService.java
+++ src/main/java/com/coffee/gifu/service/OfferService.java
@@ -2,6 +2,7 @@ package com.coffee.gifu.service;
 
 import com.coffee.gifu.service.dto.OfferDTO;
 import com.coffee.gifu.service.exception.ManagementRulesException;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,9 +37,29 @@ public interface OfferService {
     Optional<OfferDTO> findOne(Long id);
 
     /**
+     * Get the offer by account.
+     *
+     * @param id the id of the account.
+     * @return an entity list.
+     */
+    Optional<OfferDTO> findOnebyAccount(Long id);
+
+    /**
      * Delete the "id" offer.
      *
      * @param id the id of the entity.
      */
     void delete(Long id);
+
+
+    List<OfferDTO> findAllSelect(boolean freez);
+
+    Optional<OfferDTO> findAllCreate();
+
+    /**
+     * Get all the available offers.
+     *
+     * @return the list of available entities.
+     */
+    Optional<OfferDTO> findAllAvailable();
 }
